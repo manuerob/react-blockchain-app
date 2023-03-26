9c9b7a28cd51a6c9bbf91178135bd72d3db3ba8a
manuerob <emmanuelle.roberge@polymtl.ca>
2023-03-26 16:18:07 +0000

    All sections of website

---
+++ react-vite/src/components/ClaritySection.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+
+const ClaritySection = () => {
+
+  return (
+    <div>
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.sectionSubText}>Built for Bitcoin and Stacks</p>
+        <h3 className={styles.sectionHeadText}>Clarity Projects.</h3>
+
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(ClaritySection, "clarity_section");
--- react-vite/src/components/index.js
+++ react-vite/src/components/index.js
@@ -1,12 +1,22 @@
+
 import { Model3DCanvas, StarsCanvas } from './canvas';
-import Hero from './Hero';
-import Navbar from './Navbar';
-import About from './About';
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Quotes from "./Quotes";
+import CanvasLoader from "./Loader";
+import ClaritySection from "./ClaritySection";
+import SoliditySection from "./SoliditySection";
+
 
 export {
   Hero,
   Navbar,
   About,
+  Quotes,
+  ClaritySection,
+  SoliditySection,
+  CanvasLoader,
   Model3DCanvas, 
   StarsCanvas
-}
+};

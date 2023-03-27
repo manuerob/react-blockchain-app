80ccaed28c3fc8ee8cc0dc9c6fd35c197d0115ac
manuerob <emmanuelle.roberge@polymtl.ca>
2023-03-27 16:50:22 +0000

    Quotes mvp

--- react-vite/src/constants/index.js
+++ react-vite/src/constants/index.js
@@ -14,30 +14,109 @@
     ];
 
     const quotes = [
-        {
-          quote:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tristique velit. Nulla condimentum facilisis euismod.",
-          name: "Yolo duda",
-          designation: "CEO",
-          company: "Cool company",
-          image: "https://randomuser.me/api/portraits/women/4.jpg",
-        },
-        {
-          quote:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tristique velit. Nulla condimentum facilisis euismod.",
-          name: "Yolo duda 2",
-          designation: "CEO",
-          company: "boss company",
-          image: "https://randomuser.me/api/portraits/women/3.jpg",
-        },
-        {
-          quote:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tristique velit. Nulla condimentum facilisis euismod.",
-          name: "Yolo dude",
-          designation: "Janitor",
-          company: "Cool company 2",
-          image: "https://randomuser.me/api/portraits/men/5.jpg",
-        },
+      {
+        "number": 1,
+        "quote": "The darkest places in hell are reserved for those who maintain their neutrality in times of moral crisis.",
+        "author": "Dante Alighieri"
+      },
+      {
+        "number": 2,
+        "quote": "There are things that go bump in the night, and we are the ones who bump back.",
+        "author": "Hellboy"
+      },
+      {
+        "number": 3,
+        "quote": "Beware; for I am fearless, and therefore powerful.",
+        "author": "Mary Shelley, Frankenstein"
+      },
+      {
+        "number": 4,
+        "quote": "In the end, we all become stories.",
+        "author": "Margaret Atwood"
+      },
+      {
+        "number": 5,
+        "quote": "Monsters are real, and ghosts are real too. They live inside us, and sometimes, they win.",
+        "author": "Stephen King"
+      },
+      {
+        "number": 6,
+        "quote": "The oldest and strongest emotion of mankind is fear, and the oldest and strongest kind of fear is fear of the unknown.",
+        "author": "H.P. Lovecraft"
+      },
+      {
+        "number": 7,
+        "quote": "Death is not the greatest loss in life. The greatest loss is what dies inside us while we live.",
+        "author": "Norman Cousins"
+      },
+      {
+        "number": 8,
+        "quote": "We are all in the gutter, but some of us are looking at the stars.",
+        "author": "Oscar Wilde"
+      },
+      {
+        "number": 9,
+        "quote": "Hell is empty, and all the devils are here.",
+        "author": "William Shakespeare"
+      },
+      {
+        "number": 10,
+        "quote": "The only thing we have to fear is fear itself.",
+        "author": "Franklin D. Roosevelt"
+      },
+      {
+        "number": 11,
+        "quote": "There is no terror in the bang, only in the anticipation of it.",
+        "author": "Alfred Hitchcock"
+      },
+      {
+        "number": 12,
+        "quote": "I have seen the dark universe yawning, where the black planets roll without aim.",
+        "author": "H.P. Lovecraft"
+      },
+      {
+        "number": 13,
+        "quote": "The scariest monsters are the ones that lurk within our souls.",
+        "author": "Edgar Allan Poe"
+      },
+      {
+        "number": 14,
+        "quote": "We make our own monsters, then fear them for what they show us about ourselves.",
+        "author": "Mike Carey"
+      },
+      {
+        "number": 15,
+        "quote": "All that we see or seem is but a dream within a dream.",
+        "author": "Edgar Allan Poe"
+      },
+      {
+        "number": 16,
+        "quote": "Sometimes the world of the living gets mixed up with the world of the dead.",
+        "author": "Susan Hill"
+      },
+      {
+        "number": 17,
+        "quote": "The most terrifying thing is to accept oneself completely.",
+        "author": "Carl Jung"
+      },
+
+      {
+        "number": 18,
+        "quote": "They say when you meet the love of your life, time stops, and that's true.",
+        "author": "Big Fish"
+      },
+      {
+        "number": 19,
+        "quote": "There is something at work in my soul, which I do not understand.",
+        "author": "Mary Shelley, Frankenstein"
+      },
+      {
+        "number": 20,
+        "quote": "There are two types of people in the world: those who like horror movies and those who are lying.",
+        "author": "Linda Williams"
+      },
+      
       ];
+    
 
       export {  quotes };

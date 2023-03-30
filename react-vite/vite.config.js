1d46092851298d745070834ca238b82ff332aee9
manuerob <emmanuelle.roberge@polymtl.ca>
2023-03-30 01:20:27 -0400

    test11

--- react-vite/vite.config.js
+++ react-vite/vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ["@safe-global/safe-ethers-adapters",
+      "@safe-globalThis/safe-core-sdk",
+      "@safe-globalThis/safe-ethers-adapters",
       "@safe-globalThis/safe-core-sdk"]
     }
   }
